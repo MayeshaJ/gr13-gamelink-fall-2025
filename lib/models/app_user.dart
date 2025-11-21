6825bd6c24b2963daf7dcadc0dfca19f62395184
mgsharier <mgsharier@mun.ca>
2025-11-21 17:27:32 -0330

    Initialized firebase storage, updated firebase storage rules, and profile photo upload, change or delete option

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -2,11 +2,13 @@ class AppUser {
   final String uid;
   final String email;
   final String name;
+  final String photoUrl;
 
   AppUser({
     required this.uid,
     required this.email,
     required this.name,
+    required this.photoUrl,
   });
 
   Map<String, dynamic> toMap() {
@@ -14,6 +16,7 @@ class AppUser {
       'uid': uid,
       'email': email,
       'name': name,
+      'photoUrl': photoUrl,
     };
   }
 
@@ -22,6 +25,7 @@ class AppUser {
       uid: data['uid'] ?? '',
       email: data['email'] ?? '',
       name: data['name'] ?? '',
+      photoUrl: data['photoUrl'] ?? '',
     );
   }
 }
